d7262170c2c723673548e5a224b500ac7f377a49
Take434 <timh4376@gmail.com>
2023-12-16 11:57:46 +0100

    implemented tim giver

--- Assets/Scripts/MusicSynth.cs
+++ Assets/Scripts/MusicSynth.cs
@@ -11,35 +11,37 @@ public class NewBehaviourScript : MonoBehaviour
     public float _frequency { get; set; }//middle C
     private float amplitude = 0.5f;
 
+    private float _time = 0.0f;
+
     void Awake()
     {
         _sampleRate = AudioSettings.outputSampleRate;
+        Debug.Log("Sample Rate: " + _sampleRate);
     }
 
     private void OnAudioFilterRead(float[] data, int channels)
     {
         double phaseIncrement = _frequency / _sampleRate;
-        float noteLength = 0.5f;
-        float totalTime = noteLength * 2;
-        float currentTime = DateTime.Now.Second % totalTime;
 
         for (int i = 0; i < data.Length; i += channels)
         {
             float value  = 0;
 
-            if(_phase >= 50.0f) {
+            if(_time < 10f) {
                 value = Mathf.Sin((float)_phase * 2 * Mathf.PI) * amplitude;
             }
 
-            if(_phase >= 100.0f) {
-                _phase = 0.0f;
+            if(_time > 20f) {
+                _time = 0f;
             }
 
-            _phase = _phase + phaseIncrement;
+            _phase = (_phase + phaseIncrement) % 1;
 
             for(int c = 0; c < channels; c++) {
                 data[i + c] = value;
             }
         }
+
+        _time += (float)(data.Length / _sampleRate / 2);
     }
 }
